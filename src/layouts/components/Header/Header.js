6917a59bd227cce4716eb1830f525f9c4fa7e4a6
nhluan <nhluan.official@gmail.com>
2023-05-25 23:01:21 +0700

    fix header logic admin && done history exam get api

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -19,7 +19,10 @@ const Header = () => {
   }
 
   const checkLogin = window.localStorage.getItem("signin");
-  console.log("checkHeader: ", checkLogin);
+  const data = JSON.parse(checkLogin);
+  console.log("Data header: ", data);
+  console.log("Email: ", data.data.email);
+  // console.log("checkHeader: ", checkLogin);
   if (checkLogin == null) {
     return (
       <div className={styles.wrapper}>
@@ -43,42 +46,83 @@ const Header = () => {
     );
   }
   else {
-    return (
-      <div className={styles.wrapper}>
-        {/* <a href="http://localhost:3000/"> */}
-        <a onClick={() => { navigate("/") }}>
-          <img src={images.logo} alt="logo" width={50} height={50} />
-        </a>
-        <div className={styles.navLeft}>
-          {/* <a className={styles.button} href="http://localhost:3000/fulltest">Đề thi Online</a>
-          <a className={styles.button} href="http://localhost:3000/minitest">Luyện tập kỹ năng</a>
-          <a className={styles.button} href="http://localhost:3000/blog">Bài Blog</a>
-          <a className={styles.button} href="#">Ghi chú</a> */}
+    if (data.data.email !== "caoduc4work@gmail.com") {
+      return (
+        <div className={styles.wrapper}>
+          {/* <a href="http://localhost:3000/"> */}
+          <a onClick={() => { navigate("/") }}>
+            <img src={images.logo} alt="logo" width={50} height={50} />
+          </a>
+          <div className={styles.navLeft}>
+            {/* <a className={styles.button} href="http://localhost:3000/fulltest">Đề thi Online</a>
+            <a className={styles.button} href="http://localhost:3000/minitest">Luyện tập kỹ năng</a>
+            <a className={styles.button} href="http://localhost:3000/blog">Bài Blog</a>
+            <a className={styles.button} href="#">Ghi chú</a> */}
 
-          <a className={styles.button} onClick={() => { navigate("/fulltest") }}>Đề thi Online</a>
-          <a className={styles.button} onClick={() => { navigate("/minitest") }}>Luyện tập kỹ năng</a>
-          <a className={styles.button} onClick={() => { navigate("/blog") }}>Bài Blog</a>
-          <a className={styles.button} href="#">Ghi chú</a>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic" size="lg"
-              title="Drop large">
-              Hi, USER!!!
-            </Dropdown.Toggle>
+            <a className={styles.button} onClick={() => { navigate("/fulltest") }}>Đề thi Online</a>
+            <a className={styles.button} onClick={() => { navigate("/minitest") }}>Luyện tập kỹ năng</a>
+            <a className={styles.button} onClick={() => { navigate("/blog") }}>Bài Blog</a>
+            <a className={styles.button} href="#">Ghi chú</a>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic" size="lg"
+                title="Drop large">
+                Hi, USER!!!
+              </Dropdown.Toggle>
 
-            <Dropdown.Menu className={styles.sizeXL}>
-              {/* <Dropdown.Item href="http://localhost:3000/profile">Profile</Dropdown.Item> */}
-              <Dropdown.Item onClick={() => { navigate("/profile") }}>Profile</Dropdown.Item>
-              {/* <Dropdown.Item onClick={logOut} >Đăng xuất</Dropdown.Item> */}
-              <Dropdown.Item onClick={() => { navigate("/signin") }} >Đăng xuất</Dropdown.Item>
-
-
-            </Dropdown.Menu>
-          </Dropdown>
+              <Dropdown.Menu className={styles.sizeXL}>
+             
+                <Dropdown.Item onClick={() => { navigate("/profile") }}>Profile</Dropdown.Item>
+                {/* <Dropdown.Item onClick={logOut} >Đăng xuất</Dropdown.Item> */}
+                <Dropdown.Item onClick={() => { navigate("/signin") }} >Đăng xuất</Dropdown.Item>
 
 
+              </Dropdown.Menu>
+            </Dropdown>
+
+
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div className={styles.wrapper}>
+          {/* <a href="http://localhost:3000/"> */}
+          <a onClick={() => { navigate("/") }}>
+            <img src={images.logo} alt="logo" width={50} height={50} />
+          </a>
+          <div className={styles.navLeft}>
+            {/* <a className={styles.button} href="http://localhost:3000/fulltest">Đề thi Online</a>
+            <a className={styles.button} href="http://localhost:3000/minitest">Luyện tập kỹ năng</a>
+            <a className={styles.button} href="http://localhost:3000/blog">Bài Blog</a>
+            <a className={styles.button} href="#">Ghi chú</a> */}
+
+            <a className={styles.button} onClick={() => { navigate("/fulltest") }}>Đề thi Online</a>
+            <a className={styles.button} onClick={() => { navigate("/minitest") }}>Luyện tập kỹ năng</a>
+            <a className={styles.button} onClick={() => { navigate("/blog") }}>Bài Blog</a>
+            <a className={styles.button} href="#">Ghi chú</a>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic" size="lg"
+                title="Drop large">
+                Hi, ADMIN!!!
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className={styles.sizeXL}>
+                {/* <Dropdown.Item href="http://localhost:3000/profile">Profile</Dropdown.Item> */}
+                <Dropdown.Item onClick={() => { navigate("/admin/managetest") }}>Profile</Dropdown.Item>
+                {/* <Dropdown.Item onClick={logOut} >Đăng xuất</Dropdown.Item> */}
+                <Dropdown.Item onClick={() => { navigate("/signin") }} >Đăng xuất</Dropdown.Item>
+
+
+              </Dropdown.Menu>
+            </Dropdown>
+
+
+          </div>
+        </div>
+      );
+    }
+
 
   }
 
--- src/pages/HistoryExam/HistoryExam.js
+++ src/pages/HistoryExam/HistoryExam.js
@@ -10,9 +10,12 @@ import { Await, useNavigate } from "react-router-dom";
 function HistoryExam() {
 
     const checkLogin = JSON.parse(window.localStorage.getItem("signin"));
-    const userId = checkLogin.data.userId; 
-    console.log("checkLogin: ", checkLogin );
+    const userId = checkLogin.data.userId;
+    console.log("checkLogin: ", checkLogin);
     console.log("userID: ", userId);
+    var checkArray = true;
+
+    const [dataHistory, setDataHistory] = useState([]);
 
 
 
@@ -22,116 +25,120 @@ function HistoryExam() {
                 const res = await axios.get(
                     `http://tinhoccaogiaphat.com/tests/history/${userId}`
                 )
+                // console.log("Date History: ", res.data);
+                setDataHistory(res.data.data);
 
-                console.log("data: ", res.data);
             } catch (error) {
                 console.log(error.message);
-                            }
+            }
         }
-
-        getHistory(); 
-    })
-
+        getHistory();
+    }, [])
 
 
+    // console.log("History Data Real: ", dataHistory);
 
-
-    const data = [
-        {
-            date: "18:00 15/05/2023",
-            nameExam: "ETS TOEIC 2022 TEST 1",
-            score: "650"
-        },
-        {
-            date: "7:00 19/05/2023",
-            nameExam: "ETS TOEIC 2022 TEST 2",
-            score: "595"
-        },
-        {
-            date: "13:00 20/05/2023",
-            nameExam: "ETS TOEIC 2022 TEST 3",
-            score: "655"
-        },
-        {
-            date: "17:00 21/05/2023",
-            nameExam: "ETS TOEIC 2022 TEST 4",
-            score: "700"
-        },
-        {
-            date: "20:00 24/05/2023",
-            nameExam: "ETS TOEIC 2022 TEST 5",
-            score: "725"
-        }
-    ]
     const navigate = useNavigate();
-    return (
-        <div className={style.wrapper}>
-            <h1>Cài đặt</h1>
-            <hr></hr>
-            <div className={style.mainProfile}>
-                <div className={style.tabContent}>
-                    <div>
-                        {/* <a href="http://localhost:3000/profile">Thông tin</a> */}
-                        <a onClick={()=>{navigate("/profile")}}>Thông tin</a>
-                        <h3 className={style.ClickedTab}>Danh sách bài thi</h3>
-                        <h3>Thông báo</h3>
-                        <h3>Feedback</h3>
-                        <h3>Báo cáo lỗi</h3>
+    if (dataHistory.length !== 0) {
+        //empty -> don't exam 
+        return (
+            <div className={style.wrapper}>
+                <h1>Cài đặt</h1>
+                <hr></hr>
+                <div className={style.mainProfile}>
+                    <div className={style.tabContent}>
+                        <div>
+                            {/* <a href="http://localhost:3000/profile">Thông tin</a> */}
+                            <a onClick={() => { navigate("/profile") }}>Thông tin</a>
+                            <h3 className={style.ClickedTab}>Danh sách bài thi</h3>
+                            <h3>Thông báo</h3>
+                            <h3>Feedback</h3>
+                            <h3>Báo cáo lỗi</h3>
+                        </div>
+                        <div className={style.verticalLine}></div>
                     </div>
-                    <div className={style.verticalLine}></div>
+
+                    <div className={style.infor}>
+                        {/* header in main profile */}
+                        <Table striped bordered hover size="xxl">
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "15rem" }}>STT</th>
+                                    <th>Thời gian</th>
+                                    <th>Đề thi</th>
+                                    <th style={{ width: "15rem" }}>Kết quả</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {dataHistory.map((item, index) => {
+                                    return (
+                                        <tr>
+                                            <td>{index + 1}</td>
+                                            <td>{item.timeStart}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.score}</td>
+                                        </tr>
+                                    )
+                                })}
+
+                            </tbody>
+                        </Table>
+                    </div>
+
+
+
                 </div>
-
-                <div className={style.infor}>
-                    {/* header in main profile */}
-                    <Table striped bordered hover size="xxl">
-                        <thead>
-                            <tr>
-                                <th style={{ width: "15rem" }}>STT</th>
-                                <th>Thời gian</th>
-                                <th>Đề thi</th>
-                                <th style={{width: "15rem"}}>Kết quả</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((item, index) => {
-                                return (
-                                    <tr>
-                                        <td>{index + 1}</td>
-                                        <td>{item.date}</td>
-                                        <td>{item.nameExam}</td>
-                                        <td>{item.score}</td>
-                                    </tr>
-                                )
-                            })}
-                            {/* <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Thornton</td>
-                                <td>Thornton</td>
-                                <td>@twitter</td>
-                            </tr> */}
-                        </tbody>
-                    </Table>
-                </div>
-
-
-
             </div>
-        </div>
 
 
-    )
+        )
+    }
+    else {
+        return (
+            <div className={style.wrapper}>
+                <h1>Cài đặt</h1>
+                <hr></hr>
+                <div className={style.mainProfile}>
+                    <div className={style.tabContent}>
+                        <div>
+                            {/* <a href="http://localhost:3000/profile">Thông tin</a> */}
+                            <a onClick={() => { navigate("/profile") }}>Thông tin</a>
+                            <h3 className={style.ClickedTab}>Danh sách bài thi</h3>
+                            <h3>Thông báo</h3>
+                            <h3>Feedback</h3>
+                            <h3>Báo cáo lỗi</h3>
+                        </div>
+                        <div className={style.verticalLine}></div>
+                    </div>
+
+                    <div className={style.infor}>
+                        {/* header in main profile */}
+                        <Table striped bordered hover size="xxl">
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "15rem" }}>STT</th>
+                                    <th>Thời gian</th>
+                                    <th>Đề thi</th>
+                                    <th style={{ width: "15rem" }}>Kết quả</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td colSpan={4}>Chưa tham gia thi Toeic</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+
+        )
+    }
+
 
 }
 
