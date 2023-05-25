596d5db69feea260b424a0eb84d004cf27c25b35
Huỳnh Ngọc Sơn 20127614 <sonhuynh.08092002@gmail.com>
2023-05-25 23:00:39 +0700

    fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,11 @@ export { default as DoMiniTest } from "./DoMiniTest";
 export { default as MiniTest } from "./MiniTest";
 export { default as ChooseMiniTest } from "./ChooseMiniTest";
 export { default as Result } from "./Result";
-export {default as SignIn} from "./SignIn";
-export {default as SignUp} from "./SignUp";
-export {default as Admin} from "./Admin";
-export {default as Blog} from "./Blog";
-export {default as DetailBlog} from "./DetailBlog";
+export { default as SignIn } from "./SignIn";
+export { default as SignUp } from "./SignUp";
+export { default as Admin } from "./Admin";
+export { default as Blog } from "./Blog";
+export { default as DetailBlog } from "./DetailBlog";
 
 export { default as ResultDetail } from "./ResultDetail";
 export { default as ResultDetailMiniTest } from "./ResultDetailMiniTest";
